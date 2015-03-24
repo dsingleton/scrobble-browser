740ae536ac42310d6d318e8597316622c2258105
David Singleton <david@dsingleton.co.uk>
2015-03-24 22:11:15 +0000

    More nesting, scrobbles on everything, with cleaner scoping
    Simpler than charts as theres not a group/count step, but this model seems to
    work a lot better/more simply, eg switching by resource in the controller
    and calling the relation method on the resource, rather than chaining
    on a common relation object, trying to keep everything in scope.
    
    Also adds some smart display logic in scroble/chart views, to hide columns
    that are part of the scope, eg no artist on an artist top tracks view.
    
    Same for simple breadcrumb/context links, just on scrobble view
    as a example for now

--- app/models/scrobble.rb
+++ app/models/scrobble.rb
@@ -7,7 +7,6 @@ class Scrobble < ActiveRecord::Base
   validates :user, :track, :listened_at, presence: true
 
   scope :recent, -> { order(listened_at: :desc) }
-  scope :with_joins, -> { includes(:user, :track, :artist) }
 
   def self.by_user(user)
     where(user: user)
