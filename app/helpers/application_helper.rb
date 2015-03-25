c5856f6da851b6afd2b99c3700204dc29037fd1c
David Singleton <david@dsingleton.co.uk>
2015-03-25 22:35:17 +0000

    Improved chart-style view styling
    - Fix chart position for non-first pages
    - Add chart bar as a percetage of first item to show relative
      popularity of item
    - Add consistent classes to table headers, to set more consitent
      column widths when columns are hidden based on context

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def build_chart(item, chart)
+    pc = item.plays.to_f / chart.first.plays * 100
+    content_tag :span, class: "bar", style: "width: #{pc}%" do
+      number_with_delimiter(item.plays)
+    end
+  end
 end
