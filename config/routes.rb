57437f9c4c0ba185b5a898bd0480f28d9bdc8b21
David Singleton <david@dsingleton.co.uk>
2015-03-27 23:05:39 +0000

    Refactor routing to reduce nesting duplication
    Create concerns for the commonly nested resources, to DRY up
    the limiting of action to index only, and to try and make
    the nested/relationships between resource a little more readable.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:index, :show] do
-    resources :scrobbles, only: [:index]
-    resources :artists, only: [:index]
-    resources :tracks, only: [:index]
-  end
+  concern :users      do resources :users,     only: [:index] end
+  concern :artists    do resources :artists,   only: [:index] end
+  concern :tracks     do resources :tracks,    only: [:index] end
+  concern :scrobbles  do resources :scrobbles, only: [:index] end
+
+  resources :users, only: [:index, :show], concerns: [:scrobbles, :artists, :tracks]
 
   constraints(id: /.+/) do
-    resources :artists, only: [:index, :show] do
-      resources :users, only: [:index]
-      resources :scrobbles, only: [:index]
-
-      resources :tracks, only: [:index, :show] do
-        resources :users, only: [:index]
-        resources :scrobbles, only: [:index]
-      end
+    resources :artists, only: [:index, :show], concerns: [:scrobbles, :users] do
+      resources :tracks, only: [:index, :show], concerns: [:scrobbles, :users]
     end
   end
 
