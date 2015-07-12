06ff31e614e6795dce0a38414668da3aef0c0778
David Singleton <david@dsingleton.co.uk>
2015-07-12 12:37:28 +0100

    Fix accidental commit that was already merged :/

--- test/unit/scrobbler_importer_test.rb
+++ test/unit/scrobbler_importer_test.rb
@@ -30,24 +30,4 @@ class ScrobbleImporterTest < ActiveSupport::TestCase
     assert_equal '598bc2a9-1a27-4f6a-a7cc-30649303edca', first_scrobble.track.mbid
   end
 
-  test "Importing raw scrobbles are created" do
-
-    scrobble_data = [
-      ['2015-03-08T14:13:00+00:00', 1425823980, 'Wildest Dreams', '598bc2a9-1a27-4f6a-a7cc-30649303edca', 'Taylor Swift', '20244d07-534f-4eff-b4d4-930878889970', '', '', '', '', '1989 (Deluxe)', '', '', 'spotify'],
-    ]
-
-    user = User.create(username: 'tester')
-
-    importer = ScrobbleImporter.new
-    importer.import(user, scrobble_data)
-
-    scrobble = Scrobble.last
-    raw_scrobble = scrobble.raw
-
-    assert_equal 'spotift', raw_scrobble.application second_scrobble.listened_at
-    assert_equal 'Taylor Swift', second_scrobble.artist.name
-    assert_equal '20244d07-534f-4eff-b4d4-930878889970', first_scrobble.artist.mbid
-    assert_equal 'Wildest Dreams', second_scrobble.track.name
-    assert_equal '598bc2a9-1a27-4f6a-a7cc-30649303edca', first_scrobble.track.mbid
-  end
 end
